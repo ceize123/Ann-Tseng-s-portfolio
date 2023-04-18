7a9002f9d407c8cb07defddc5308b9a6ef11586a
Enzo <yitsokuo19@gmail.com>
2023-04-17 21:41:58 -0400

    test pixel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,33 +6,40 @@ import Image from 'next/image';
 import bgUrl from 'assets/images/bg.png';
 import { cn } from 'helpers';
 
+const ary = Array(100)
+  .fill(null)
+  .map((_, i) => i);
+const randomDark = ary.sort(() => 0.5 - Math.random()).slice(0, 20);
+const randomLight = ary.sort(() => 0.5 - Math.random()).slice(0, 15);
+
 export const Home: React.FC = () => {
   const [reached, setReached] = useState<boolean>(false);
   const [trigger, setTrigger] = useState<number>(0);
   const [rate, setRate] = useState<number>(0);
   const pixelRef = useRef<HTMLDivElement | null>(null);
 
-  // const [randomDark, setRandomDark] = useState<number[]>(ary);
-  // const [randomLight, setRandomLight] = useState<number[]>(ary);
-  const generateAry = useMemo(() => {
-    const ary = Array(100)
-      .fill(null)
-      .map((_, i) => i);
-    const randomDark = ary.sort(() => 0.5 - Math.random()).slice(0, 20);
-    const randomLight = ary.sort(() => 0.5 - Math.random()).slice(0, 10);
-    return { ary, randomDark, randomLight };
+  // const generatePixels = useMemo(() => {
+  //   const randomDark = ary.sort(() => 0.5 - Math.random()).slice(0, 20);
+  //   const randomLight = ary.sort(() => 0.5 - Math.random()).slice(0, 10);
+  //   return { ary, randomDark, randomLight };
+  // }, [trigger]);
+  useMemo(() => {
+    ary.sort(() => 0.5 - Math.random());
+    return { ary };
   }, [trigger]);
 
   useEffect(() => {
     const scroll = () => {
-      setRate(window.scrollY / 1500);
       const aboutSecTop = pixelRef?.current?.getBoundingClientRect()['top'];
       if (aboutSecTop !== undefined && aboutSecTop < 0) {
         setReached(true);
-        if (aboutSecTop % 15 === 0) {
+        if (aboutSecTop % 8 === 0) {
           setTrigger(aboutSecTop);
         }
-      } else setReached(false);
+      } else {
+        setReached(false);
+        setRate(window.scrollY / 1500);
+      }
     };
     window.addEventListener('scroll', scroll);
     return () => window.removeEventListener('scroll', scroll);
@@ -50,7 +57,7 @@ export const Home: React.FC = () => {
           className='w-full h-auto'
           style={{
             transform: `scale(${rate + 1})`,
-            opacity: rate < 0.5 ? 1 - rate : 0.5,
+            opacity: 1 - rate,
           }}
         />
       </div>
@@ -60,7 +67,7 @@ export const Home: React.FC = () => {
           !reached ? 'opacity-0' : 'opacity-100'
         )}
       >
-        {generateAry.ary.map((i) => {
+        {ary.map((i) => {
           return (
             <div
               key={i}
@@ -70,9 +77,9 @@ export const Home: React.FC = () => {
               )}
               style={{
                 backgroundColor: `${
-                  generateAry.randomDark.includes(i)
+                  randomDark.includes(i)
                     ? 'rgba(0, 0, 0, 0.5)'
-                    : generateAry.randomLight.includes(i)
+                    : randomLight.includes(i)
                     ? 'rgba(85, 85, 85, 0.5)'
                     : 'rgba(34, 34, 34, 0.5)'
                 }`,
