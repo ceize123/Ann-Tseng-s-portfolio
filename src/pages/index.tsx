eae3d4263312903af3d41c38ba890a8d6a0ebfa2
Enzo <yitsokuo19@gmail.com>
2023-05-01 22:44:43 -0400

    test pixel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Hero from 'components/Home/Hero';
 import Quote from 'components/Home/Quote';
 import Resume from 'components/Home/Resume';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import Image from 'next/image';
 import bgUrl from 'assets/images/bg.png';
 import { cn } from 'helpers';
@@ -9,33 +9,49 @@ import { cn } from 'helpers';
 const ary = Array(100)
   .fill(null)
   .map((_, i) => i);
-const colors = { randomDark: ary.slice(0, 20), randomLight: ary.slice(20, 35) };
-ary.sort(() => 0.5 - Math.random());
+// const colors = { randomDark: ary.slice(0, 20), randomLight: ary.slice(20, 35) };
+// ary.sort(() => 0.5 - Math.random());
 
 export const Home: React.FC = () => {
   const [reached, setReached] = useState<boolean>(false);
-  const [triggerAry, setTriggerAry] = useState<{
-    start: number;
-    end: number;
-  }>({ start: 0, end: 100 });
+  const [colors, setColors] = useState<{
+    randomDark: number[];
+    randomLight: number[];
+  }>({
+    randomDark: ary.slice(0, 20),
+    randomLight: ary.slice(20, 35),
+  });
+  const [trigger, setTrigger] = useState<boolean>(false);
+  // const [triggerAry, setTriggerAry] = useState<{
+  //   start: number;
+  //   end: number;
+  // }>({ start: 0, end: 100 });
   const [rate, setRate] = useState<number>(0);
   const pixelRef = useRef<HTMLDivElement | null>(null);
 
-  // const generatePixels = useMemo(() => {
-  //   const randomDark = ary.sort(() => 0.5 - Math.random()).slice(0, 20);
-  //   const randomLight = ary.sort(() => 0.5 - Math.random()).slice(0, 10);
-  //   return { ary, randomDark, randomLight };
-  // }, [trigger]);
+  useMemo(() => {
+    if (trigger) {
+      setColors({
+        randomDark: ary.slice(0, 20),
+        randomLight: ary.slice(20, 35),
+      });
+      ary.sort(() => 0.5 - Math.random());
+    }
+    return colors;
+  }, [trigger]);
 
   useEffect(() => {
     const scroll = () => {
       const aboutSecTop = pixelRef?.current?.getBoundingClientRect()['top'];
       if (aboutSecTop !== undefined && aboutSecTop < 0) {
         setReached(true);
-        if (aboutSecTop % 8 === 0) {
-          const start = Math.floor(Math.random() * 20);
-          const end = Math.floor(Math.random() * 59) + 40;
-          setTriggerAry({ start: start, end: end });
+        if (aboutSecTop % 15 === 0) {
+          setTrigger(true);
+          // const start = Math.floor(Math.random() * 20);
+          // const end = Math.floor(Math.random() * 59) + 40;
+          // setTriggerAry({ start: start, end: end });
+        } else {
+          setTrigger(false);
         }
       } else {
         setReached(false);
@@ -73,20 +89,20 @@ export const Home: React.FC = () => {
             <div
               key={i}
               className={cn(
-                'w-[10%] transition-all duration-500 backdrop-blur-md',
-                !reached || triggerAry.start > i || triggerAry.end < i
-                  ? 'opacity-0'
-                  : 'opacity-100'
+                'w-[10%] transition-opacity duration-500 backdrop-blur-md',
+                !reached ? 'opacity-0' : 'opacity-100',
+                colors.randomDark.includes(i) ? 'bg-black' : '',
+                colors.randomLight.includes(i) ? 'bg-gray' : ''
               )}
-              style={{
-                backgroundColor: `${
-                  colors.randomDark.includes(i)
-                    ? 'rgba(0, 0, 0, 0.5)'
-                    : colors.randomLight.includes(i)
-                    ? 'rgba(85, 85, 85, 0.5)'
-                    : 'rgba(34, 34, 34, 0.5)'
-                }`,
-              }}
+              // style={{
+              //   backgroundColor: `${
+              //     colors.randomDark.includes(i)
+              //       ? 'rgba(0, 0, 0, 0.5)'
+              //       : colors.randomLight.includes(i)
+              //       ? 'rgba(85, 85, 85, 0.5)'
+              //       : 'rgba(34, 34, 34, 0.5)'
+              //   }`,
+              // }}
             ></div>
           );
         })}
